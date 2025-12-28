47a44e28bf02920a21a418d684fccd6cc453c091
Saurav <Sauravtandon99@gmail.com>
2025-12-28 18:01:25 +0530

    New Updated from 5

--- Tech/src/LearnEclipse.java
+++ Tech/src/LearnEclipse.java
@@ -2,6 +2,8 @@
 public class LearnEclipse {
 	public static void main(String[] args) {
 		System.out.println("New file from LearnEclipse");
+		System.out.println("New Line to check updated 5");
 	}
+	
 
 }
