c325999e7e3d0478228aded85b3d6a28b0baf549
sauravtandon99 <sauravtandon99@gmail.com>
2025-12-28 12:44:59 +0530

    Update Python.java

--- Tech/src/Python.java
+++ Tech/src/Python.java
@@ -4,6 +4,8 @@ public class Python {
 		String str = "This is 2nd class";
 		System.out.println(str);
 		System.out.println("Adding new line to check");
+		System.out.println("New Line 2 Added ");
+		
 	}
 
 }
